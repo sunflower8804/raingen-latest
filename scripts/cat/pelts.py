0bcca0bfd926e4fc29eea8a8ee1f506b8736cf35
flamedash2358 <lucykooistra@icloud.com>
2025-09-06 01:32:01 -0700

    Update pelts.py

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -652,10 +652,11 @@ class Pelt:
     colorwings = Pelt.with_suffix(['WATCHER', 'ARTIFICER','HUNTER', 'SAINT', 'RIVULETWINGS', 'SPEARMASTER', 'GOURMAND'], 'WINGS')
     whitefadewings = Pelt.with_suffix(['WHITE','DARKCREAM','CREAM','OFFWHITE','GRAY','PINK','BLACK','POWDERBLUE','SPLASH','PURPLE','BLACKBERRY','SAND','CLAY','BRICK','SALMON','SEAFOAM','MINT','EVERGREEN','CRANBERRY','PEARL','ORCHID','RUBY','CORAL','TAN','LEMON','CLOVER','CYAN','VIOLET','GOLDEN'],'WINGSFADE')
     wings = Pelt.with_suffix(['WHITE','DARKCREAM','CREAM','OFFWHITE','GRAY','PINK','BLACK','POWDERBLUE','SPLASH','PURPLE','BLACKBERRY','SAND','CLAY','BRICK','SALMON','SEAFOAM','MINT','EVERGREEN','CRANBERRY','PEARL','ORCHID','RUBY','CORAL','TAN','LEMON','CLOVER','CYAN','VIOLET','GOLDEN'],'WINGS')
-skin_categories = [dragonhorns, moth, seaslugpapillae, tailfrills, thorns, glowspots, gills, tongues, lizardneedles, spikes, 
-                    lizardfins, catfishwhiskers, dragonwhiskers, quills, centipedegrowths, stinger, anglerfish, spearholes, cyanfeatures, cyanwings, 
-                    firebugpart, seaangelwings, loach, dropwig, glassback, grasssheepback, familiar, acrotail, tears, manes, overseertenna, budgiewings,
-                    conurewings, lovebirdwings, pidgeonwings, vulturewings, colorwings, whitefadewings, wings]
+    skin_categories = [dragonhorns, moth, seaslugpapillae, tailfrills, thorns, glowspots, gills, tongues, lizardneedles, 
+                       spikes, lizardfins, catfishwhiskers, dragonwhiskers, quills, centipedegrowths, stinger, anglerfish, 
+                       spearholes, cyanfeatures, cyanwings, firebugpart, seaangelwings, loach, dropwig, glassback, grasssheepback, 
+                       familiar, acrotail, tears, manes, overseertenna, budgiewings, conurewings, lovebirdwings, pidgeonwings, 
+                       vulturewings, colorwings, whitefadewings, wings]
     
     #list for stuff that should logically be behind a cloak
     closest_skin = [
