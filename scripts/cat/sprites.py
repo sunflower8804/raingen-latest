c643a0e6dc43efd95108010939ef516013618f4d
flamedash2358 <lucykooistra@icloud.com>
2025-09-07 23:35:11 -0700

    Update sprites.py

--- scripts/cat/sprites.py
+++ scripts/cat/sprites.py
@@ -6,10 +6,9 @@ import ujson
 
 from scripts.game_structure.game_essentials import game
 
-
+image_cache = {}
 
 def load_sprite(path):
-    image_cache = {}
     if path not in image_cache:
         image_cache[path] = pygame.image.load(path).convert_alpha()
     return image_cache[path]
